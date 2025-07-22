966b1d6d3fef38a0e5001f1ec68a7fa0ff83cd14
Mitchell Jensen <mitchell.jensen@mipac.com.au>
2025-07-22 12:10:00 +1000

    refactor: split dev and prod settings files

---
+++ core/settings_development.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from .settings import *  # noqa: F403
+
+DEBUG = True
+
+DATABASES: dict[str, dict[str, str | Path]] = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
+    },
+}
