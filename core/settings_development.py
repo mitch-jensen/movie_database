b81bda74ec16e2a09872734e79d875bb65155a7a
Mitchell Jensen <mitchell.jensen@mipac.com.au>
2025-07-23 14:43:24 +1000

    ci: split compose environments + create test env that uses SQLite

--- core/settings_development.py
+++ core/settings_development.py
@@ -1,12 +1,24 @@
-from pathlib import Path
+from os import getenv
 
 from .settings import *  # noqa: F403
 
 DEBUG = True
 
-DATABASES: dict[str, dict[str, str | Path]] = {
+
+DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": "5432",
+        "OPTIONS": {
+            "pool": {
+                "min_size": 2,
+                "max_size": 4,
+                "timeout": 100,
+            },
+        },
     },
 }
