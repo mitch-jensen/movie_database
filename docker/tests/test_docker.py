c8266500cead34d1c675f256456d49325aec8fe7
Mitchell Jensen <mitchell.jensen@mipac.com.au>
2025-07-23 17:48:34 +1000

    test: improve and parametrize test suite

--- docker/tests/test_docker.py
+++ docker/tests/test_docker.py
@@ -32,25 +32,85 @@ def host(request: pytest.FixtureRequest) -> Generator[Host]:
     if type(container.id) is not str:
         pytest.fail(f"Docker container '{container}' is invalid.")
 
-    try:
-        yield testinfra.get_host("docker://" + container.id)
-    finally:
-        container.remove(force=True)
+    yield testinfra.get_host("docker://" + container.id)
+    container.remove(force=True)
 
 
-def test_production_dependencies_are_installed(host: Host):
+@pytest.mark.parametrize(
+    "dependency",
+    [
+        "Django",
+        "django-ninja",
+        "uvicorn",
+        "pydantic",
+        "psycopg",
+    ],
+)
+def test_production_dependencies_are_installed(host: Host, dependency: str):
     dependencies: dict[str, dict[str, str]] = host.pip.get_packages()
-    expected_dependencies = ["Django", "uvicorn", "django-ninja", "pydantic", "psycopg"]
-    assert set(expected_dependencies).issubset(dependencies)
+    assert dependency in dependencies
 
 
-def test_development_dependencies_are_not_installed(host: Host):
+@pytest.mark.parametrize(
+    "dependency",
+    [
+        "beartype",
+        "commitizen",
+        "deptry",
+        "model-bakery",
+        "pre-commit",
+        "pytest",
+        "pytest-django",
+        "ruff",
+    ],
+)
+def test_development_dependencies_are_not_installed(host: Host, dependency: str):
     dependencies: dict[str, dict[str, str]] = host.pip.get_packages()
-    unexpected_dependencies = ["beartype", "commitizen", "deptry", "model-bakery", "pre-commit", "pytest", "pytest-django", "ruff"]
-    assert set(unexpected_dependencies).isdisjoint(dependencies)
+    assert dependency not in dependencies
 
 
-def test_test_dependencies_are_not_installed(host: Host):
+@pytest.mark.parametrize(
+    "dependency",
+    [
+        "docker",
+        "pytest-testinfra",
+    ],
+)
+def test_test_dependencies_are_not_installed(host: Host, dependency: str):
     dependencies: dict[str, dict[str, str]] = host.pip.get_packages()
-    unexpected_dependencies = ["docker", "pytest-testinfra"]
-    assert set(unexpected_dependencies).isdisjoint(dependencies)
+    assert dependency not in dependencies
+
+
+def test_os_distribution_is_alpine(host: Host):
+    assert host.system_info.distribution == "alpine"
+
+
+def test_default_user_is_not_root_python_user(host: Host):
+    user = host.user()
+    assert user.name == "python"
+    assert user.uid == 1000
+    assert user.gid == 1000
+
+
+@pytest.mark.parametrize(
+    ("env_variable", "expected_value"),
+    [
+        ("DJANGO_SETTINGS_MODULE", "core.settings_production"),
+        ("PYTHONUNBUFFERED", "1"),
+        ("PYTHONDONTWRITEBYTECODE", "1"),
+    ],
+)
+def test_environment_variables_are_as_expected(host: Host, env_variable: str, expected_value: str):
+    environment: dict[str, str] = host.environment()
+    assert environment[env_variable] == expected_value
+
+
+# TODO @mitch-jensen: add more tests  # noqa: FIX002, TD003
+# TODO @mitch-jensen: add host.file("/app").listdir() tests to assert folder structure  # noqa: FIX002, TD003
+@pytest.mark.parametrize(
+    "file_name",
+    ["/app/requirements.txt"],
+)
+def test_files_exist(host: Host, file_name: str):
+    file = host.file(file_name)
+    assert file.exists
