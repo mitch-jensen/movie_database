dd1b778f517ba90d0d6f7efa053780dc4b84fb9c
mitch-jensen <65543196+mitch-jensen@users.noreply.github.com>
2025-08-06 11:56:14 +1000

    feat: add more API endpoints, schemas and introduce tags (#50)
    tags added for separation of endpoint groups in OpenAPI schema

--- movie_database/schema.py
+++ movie_database/schema.py
@@ -2,24 +2,53 @@ from typing import Annotated
 
 from ninja import Field, FilterSchema, ModelSchema
 
-from .models import Movie
+from .models import MediaCaseDimensions, Movie, PhysicalMedia
 
 IContainsField = Annotated[str | None, Field(None, q="__icontains")]  # pyright: ignore[reportCallIssue]
 
 
-class MovieSchema(ModelSchema):
-    """Schema for the Movie model."""
+class MediaCaseDimensionSchemaOut(ModelSchema):  # noqa: D101
+    class Meta:  # noqa: D106
+        model = MediaCaseDimensions
+        fields = ("id", "width", "height", "depth", "media_format", "description")
 
-    class Meta:
-        """Meta class for MovieSchema."""
 
+class MediaCaseDimensionSchemaIn(ModelSchema):  # noqa: D101
+    class Meta:  # noqa: D106
+        model = MediaCaseDimensions
+        fields = ("width", "height", "depth", "media_format", "description")
+
+
+class MediaCaseDimensionFilterSchema(FilterSchema):  # noqa: D101
+    title: IContainsField
+    release_year: int | None = Field(None)
+
+
+class MovieSchemaOut(ModelSchema):  # noqa: D101
+    class Meta:  # noqa: D106
         model = Movie
         fields = ("id", "title", "release_year", "letterboxd_uri", "watched")
 
 
-class MovieFilterSchema(FilterSchema):
-    """Schema for filtering movies."""
+class MovieSchemaIn(ModelSchema):  # noqa: D101
+    class Meta:  # noqa: D106
+        model = Movie
+        fields = ("title", "release_year", "letterboxd_uri", "watched")
 
+
+class PhysicalMediaSchema(ModelSchema):  # noqa: D101
+    class Meta:  # noqa: D106
+        model = PhysicalMedia
+        fields = ("position_on_shelf", "notes")
+
+
+class MovieSchemaWithMedia(MovieSchemaOut):  # noqa: D101
+    physical_media_set: list[PhysicalMediaSchema]
+
+
+class MovieFilterSchema(FilterSchema):  # noqa: D101
     title: IContainsField
-    release_year: int | None = Field(None)
+    release_year: Annotated[int | None, Field(None)]
+    release_year_lt: Annotated[int | None, Field(None, q="release_year__lt")]
+    release_year_gt: Annotated[int | None, Field(None, q="release_year__gt")]
     watched: bool | None = Field(None)
