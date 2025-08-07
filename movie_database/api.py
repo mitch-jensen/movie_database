432aa8bcc303b821875675aa082beca6cc653788
Mitchell Jensen <mitchell.jensen@mipac.com.au>
2025-08-07 10:33:04 +1000

    refactor: rename schemas to follow simplified naming convention
    don't need to include Schema in the name as they're already namespaced
    in schemas.py

--- movie_database/api.py
+++ movie_database/api.py
@@ -24,26 +24,26 @@ class DefaultDeleteSuccessResponse(TypedDict):
 
 
 @router.post("/bookcases", tags=["bookcase"])
-async def create_bookcase(request: HttpRequest, payload: schemas.BookcaseSchemaIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
+async def create_bookcase(request: HttpRequest, payload: schemas.BookcaseIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
     bookcase = await Bookcase.objects.acreate(**payload.dict())
     return {"id": bookcase.id}
 
 
-@router.get("/bookcases", response=list[schemas.BookcaseSchemaOut], tags=["bookcase"])
-async def list_bookcases(request: HttpRequest) -> list[schemas.BookcaseSchemaOut]:  # noqa: ARG001, D103
+@router.get("/bookcases", response=list[schemas.BookcaseOut], tags=["bookcase"])
+async def list_bookcases(request: HttpRequest) -> list[schemas.BookcaseOut]:  # noqa: ARG001, D103
     bookcase = Bookcase.objects.all()
-    return [schemas.BookcaseSchemaOut.from_orm(m) async for m in bookcase]
+    return [schemas.BookcaseOut.from_orm(m) async for m in bookcase]
 
 
-@router.get("/bookcases/{bookcase_id}", response=schemas.BookcaseSchemaOut, tags=["bookcase"])
+@router.get("/bookcases/{bookcase_id}", response=schemas.BookcaseOut, tags=["bookcase"])
 async def get_bookcase(request: HttpRequest, bookcase_id: int) -> Bookcase:  # noqa: ARG001, D103
     return await aget_object_or_404(Bookcase, id=bookcase_id)
 
 
-@router.get("/bookcases/{bookcase_id}/shelves", response=list[schemas.ShelfSchemaOut], tags=["bookcase", "shelf"])
-async def get_bookcase_shelves(request: HttpRequest, bookcase_id: int) -> list[schemas.ShelfSchemaOut]:  # noqa: ARG001, D103
+@router.get("/bookcases/{bookcase_id}/shelves", response=list[schemas.ShelfOut], tags=["bookcase", "shelf"])
+async def get_bookcase_shelves(request: HttpRequest, bookcase_id: int) -> list[schemas.ShelfOut]:  # noqa: ARG001, D103
     bookcase = await aget_object_or_404(Bookcase, id=bookcase_id)
-    return [schemas.ShelfSchemaOut.from_orm(shelf) async for shelf in bookcase.shelves.all()]
+    return [schemas.ShelfOut.from_orm(shelf) async for shelf in bookcase.shelves.all()]
 
 
 @router.delete("/bookcases/{bookcase_id}", tags=["bookcase"])
@@ -54,29 +54,29 @@ async def delete_bookcase(request: HttpRequest, bookcase_id: int) -> DefaultDele
 
 
 @router.post("/collections", tags=["collection"])
-async def create_collection(request: HttpRequest, payload: schemas.CollectionSchemaIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
+async def create_collection(request: HttpRequest, payload: schemas.CollectionIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
     collection = await Collection.objects.acreate(**payload.dict())
     return {"id": collection.id}
 
 
-@router.get("/collections", response=list[schemas.CollectionSchemaOut], tags=["collection"])
-async def list_collections(request: HttpRequest) -> list[schemas.CollectionSchemaOut]:  # noqa: ARG001, D103
+@router.get("/collections", response=list[schemas.CollectionOut], tags=["collection"])
+async def list_collections(request: HttpRequest) -> list[schemas.CollectionOut]:  # noqa: ARG001, D103
     collection = Collection.objects.all()
-    return [schemas.CollectionSchemaOut.from_orm(m) async for m in collection]
+    return [schemas.CollectionOut.from_orm(m) async for m in collection]
 
 
-@router.get("/collections/{collection_id}", response=schemas.CollectionSchemaOut, tags=["collection"])
+@router.get("/collections/{collection_id}", response=schemas.CollectionOut, tags=["collection"])
 async def get_collection(request: HttpRequest, collection_id: int) -> Collection:  # noqa: ARG001, D103
     return await aget_object_or_404(Collection, id=collection_id)
 
 
-@router.get("/collections/{collection_id}/media", response=list[schemas.PhysicalMediaSchema], tags=["collection", "physical_media"])
-async def get_collection_media_list(request: HttpRequest, collection_id: int) -> list[schemas.PhysicalMediaSchema]:  # noqa: ARG001, D103
+@router.get("/collections/{collection_id}/media", response=list[schemas.PhysicalMedia], tags=["collection", "physical_media"])
+async def get_collection_media_list(request: HttpRequest, collection_id: int) -> list[schemas.PhysicalMedia]:  # noqa: ARG001, D103
     collection = await aget_object_or_404(Collection, id=collection_id)
-    return [schemas.PhysicalMediaSchema.from_orm(shelf) async for shelf in collection.physical_media_set.all()]
+    return [schemas.PhysicalMedia.from_orm(shelf) async for shelf in collection.physical_media_set.all()]
 
 
-@router.get("/collections/{collection_id}/media/{media_id}", response=schemas.PhysicalMediaSchema, tags=["collection", "physical_media"])
+@router.get("/collections/{collection_id}/media/{media_id}", response=schemas.PhysicalMedia, tags=["collection", "physical_media"])
 async def get_collection_media(request: HttpRequest, collection_id: int, media_id: int) -> PhysicalMedia:  # noqa: ARG001, D103
     collection: Collection = await aget_object_or_404(Collection, id=collection_id)
     return await collection.physical_media_set.aget(id=media_id)
@@ -90,18 +90,18 @@ async def delete_collection(request: HttpRequest, collection_id: int) -> Default
 
 
 @router.post("/media_case_dimensions", tags=["media_case_dimensions"])
-async def create_media_case_dimensions(request: HttpRequest, payload: schemas.MediaCaseDimensionSchemaIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
+async def create_media_case_dimensions(request: HttpRequest, payload: schemas.MediaCaseDimensionIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
     media_case_dimensions = await MediaCaseDimensions.objects.acreate(**payload.dict())
     return {"id": media_case_dimensions.id}
 
 
-@router.get("/media_case_dimensions", response=list[schemas.MediaCaseDimensionSchemaOut], tags=["media_case_dimensions"])
-async def list_media_case_dimensions(request: HttpRequest) -> list[schemas.MediaCaseDimensionSchemaOut]:  # noqa: ARG001, D103
+@router.get("/media_case_dimensions", response=list[schemas.MediaCaseDimensionOut], tags=["media_case_dimensions"])
+async def list_media_case_dimensions(request: HttpRequest) -> list[schemas.MediaCaseDimensionOut]:  # noqa: ARG001, D103
     media_case_dimensions = MediaCaseDimensions.objects.all()
-    return [schemas.MediaCaseDimensionSchemaOut.from_orm(m) async for m in media_case_dimensions]
+    return [schemas.MediaCaseDimensionOut.from_orm(m) async for m in media_case_dimensions]
 
 
-@router.get("/media_case_dimensions/{media_case_dimensions_id}", response=schemas.MediaCaseDimensionSchemaOut, tags=["media_case_dimensions"])
+@router.get("/media_case_dimensions/{media_case_dimensions_id}", response=schemas.MediaCaseDimensionOut, tags=["media_case_dimensions"])
 async def get_media_case_dimensions(request: HttpRequest, media_case_dimensions_id: int) -> MediaCaseDimensions:  # noqa: ARG001, D103
     return await aget_object_or_404(MediaCaseDimensions, id=media_case_dimensions_id)
 
@@ -114,18 +114,18 @@ async def delete_media_case_dimensions(request: HttpRequest, media_case_dimensio
 
 
 @router.post("/shelf_dimensions", tags=["shelf_dimensions"])
-async def create_shelf_dimensions(request: HttpRequest, payload: schemas.ShelfDimensionSchemaIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
+async def create_shelf_dimensions(request: HttpRequest, payload: schemas.ShelfDimensionIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
     shelf_dimensions = await ShelfDimensions.objects.acreate(**payload.dict())
     return {"id": shelf_dimensions.id}
 
 
-@router.get("/shelf_dimensions", response=list[schemas.ShelfDimensionSchemaOut], tags=["shelf_dimensions"])
-async def list_shelf_dimensions(request: HttpRequest) -> list[schemas.ShelfDimensionSchemaOut]:  # noqa: ARG001, D103
+@router.get("/shelf_dimensions", response=list[schemas.ShelfDimensionOut], tags=["shelf_dimensions"])
+async def list_shelf_dimensions(request: HttpRequest) -> list[schemas.ShelfDimensionOut]:  # noqa: ARG001, D103
     shelf_dimensions = ShelfDimensions.objects.all()
-    return [schemas.ShelfDimensionSchemaOut.from_orm(m) async for m in shelf_dimensions]
+    return [schemas.ShelfDimensionOut.from_orm(m) async for m in shelf_dimensions]
 
 
-@router.get("/shelf_dimensions/{shelf_dimensions_id}", response=schemas.ShelfDimensionSchemaOut, tags=["shelf_dimensions"])
+@router.get("/shelf_dimensions/{shelf_dimensions_id}", response=schemas.ShelfDimensionOut, tags=["shelf_dimensions"])
 async def get_shelf_dimensions(request: HttpRequest, shelf_dimensions_id: int) -> ShelfDimensions:  # noqa: ARG001, D103
     return await aget_object_or_404(ShelfDimensions, id=shelf_dimensions_id)
 
@@ -138,24 +138,24 @@ async def delete_shelf_dimensions(request: HttpRequest, shelf_dimensions_id: int
 
 
 @router.post("/movies", tags=["movies"])
-async def create_movie(request: HttpRequest, payload: schemas.MovieSchemaIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
+async def create_movie(request: HttpRequest, payload: schemas.MovieIn) -> DefaultPostSuccessResponse:  # noqa: ARG001, D103
     movie = await Movie.objects.acreate(**payload.dict())
     return {"id": movie.id}
 
 
-@router.get("/movies", response=list[schemas.MovieSchemaOut], tags=["movies"])
-async def list_movies(request: HttpRequest, filters: Annotated[schemas.MovieFilterSchema, Query(...)]) -> list[schemas.MovieSchemaOut]:  # noqa: ARG001, D103
+@router.get("/movies", response=list[schemas.MovieOut], tags=["movies"])
+async def list_movies(request: HttpRequest, filters: Annotated[schemas.MovieFilter, Query(...)]) -> list[schemas.MovieOut]:  # noqa: ARG001, D103
     movies = Movie.objects.all()
     movies = filters.filter(movies)
-    return [schemas.MovieSchemaOut.from_orm(movie) async for movie in movies]
+    return [schemas.MovieOut.from_orm(movie) async for movie in movies]
 
 
-@router.get("/movies/{movie_id}", response=schemas.MovieSchemaOut, tags=["movies"])
+@router.get("/movies/{movie_id}", response=schemas.MovieOut, tags=["movies"])
 async def get_movie(request: HttpRequest, movie_id: int) -> Movie:  # noqa: ARG001, D103
     return await aget_object_or_404(Movie, id=movie_id)
 
 
-@router.get("/movies/{movie_id}/physical_media", response=list[schemas.PhysicalMediaSchema], tags=["movies", "physical_media"])
-async def get_movie_physical_media(request: HttpRequest, movie_id: int) -> list[schemas.PhysicalMediaSchema]:  # noqa: ARG001, D103
+@router.get("/movies/{movie_id}/physical_media", response=list[schemas.PhysicalMedia], tags=["movies", "physical_media"])
+async def get_movie_physical_media(request: HttpRequest, movie_id: int) -> list[schemas.PhysicalMedia]:  # noqa: ARG001, D103
     movie: Movie = await aget_object_or_404(Movie, id=movie_id)
-    return [schemas.PhysicalMediaSchema.from_orm(medium) async for medium in movie.physical_media_set.all()]
+    return [schemas.PhysicalMedia.from_orm(medium) async for medium in movie.physical_media_set.all()]
--- movie_database/schema.py
+++ movie_database/schema.py
@@ -7,84 +7,84 @@ import movie_database.models as movie_models
 IContainsField = Annotated[str | None, Field(None, q="__icontains")]  # pyright: ignore[reportCallIssue]
 
 
-class BookcaseSchemaOut(ModelSchema):  # noqa: D101
+class BookcaseOut(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Bookcase
         fields = ("id", "name", "description", "location")
 
 
-class BookcaseSchemaIn(ModelSchema):  # noqa: D101
+class BookcaseIn(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Bookcase
         fields = ("name", "description", "location")
 
 
-class CollectionSchemaOut(ModelSchema):  # noqa: D101
+class CollectionOut(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Collection
         fields = ("id", "name")
 
 
-class CollectionSchemaIn(ModelSchema):  # noqa: D101
+class CollectionIn(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Collection
         fields = ("name",)
 
 
-class ShelfSchemaOut(ModelSchema):  # noqa: D101
+class ShelfOut(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Shelf
         fields = ("id", "position_from_top", "orientation")
 
 
-class MediaCaseDimensionSchemaOut(ModelSchema):  # noqa: D101
+class MediaCaseDimensionOut(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.MediaCaseDimensions
         fields = ("id", "width", "height", "depth", "media_format", "description")
 
 
-class MediaCaseDimensionSchemaIn(ModelSchema):  # noqa: D101
+class MediaCaseDimensionIn(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.MediaCaseDimensions
         fields = ("width", "height", "depth", "media_format", "description")
 
 
-class MediaCaseDimensionFilterSchema(FilterSchema):  # noqa: D101
+class MediaCaseDimensionFilter(FilterSchema):  # noqa: D101
     title: IContainsField
     release_year: int | None = Field(None)
 
 
-class ShelfDimensionSchemaOut(ModelSchema):  # noqa: D101
+class ShelfDimensionOut(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.MediaCaseDimensions
         fields = ("id", "width", "height", "depth")
 
 
-class ShelfDimensionSchemaIn(ModelSchema):  # noqa: D101
+class ShelfDimensionIn(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.MediaCaseDimensions
         fields = ("width", "height", "depth")
 
 
-class MovieSchemaOut(ModelSchema):  # noqa: D101
+class MovieOut(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Movie
         fields = ("id", "title", "release_year", "letterboxd_uri", "watched")
 
 
-class MovieSchemaIn(ModelSchema):  # noqa: D101
+class MovieIn(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.Movie
         fields = ("title", "release_year", "letterboxd_uri", "watched")
 
 
-class PhysicalMediaSchema(ModelSchema):  # noqa: D101
+class PhysicalMedia(ModelSchema):  # noqa: D101
     class Meta:  # noqa: D106
         model = movie_models.PhysicalMedia
         fields = ("position_on_shelf", "notes")
 
 
-class MovieFilterSchema(FilterSchema):  # noqa: D101
+class MovieFilter(FilterSchema):  # noqa: D101
     title: IContainsField
     release_year: Annotated[int | None, Field(None)]
     release_year_lt: Annotated[int | None, Field(None, q="release_year__lt")]
